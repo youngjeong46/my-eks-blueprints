4b2f3844e303e5be4774725639a96fcc1296a557
EC2 Default User <ec2-user@ip-172-31-31-12.us-west-2.compute.internal>
2022-08-16 04:11:52 +0000

    adding teams

--- lib/pipeline.ts
+++ lib/pipeline.ts
@@ -3,6 +3,8 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as blueprints from '@aws-quickstart/eks-blueprints';
 
+import { TeamPlatform, TeamApplication } from '../teams'; // HERE WE IMPORT TEAMS
+
 export default class PipelineConstruct extends Construct {
   constructor(scope: Construct, id: string, props?: cdk.StackProps){
     super(scope,id)
@@ -15,7 +17,7 @@ export default class PipelineConstruct extends Construct {
     .account(account)
     .region(region)
     .addOns()
-    .teams();
+    .teams(new TeamPlatform(account), new TeamApplication('burnham',account));
   
     blueprints.CodePipelineStack.builder()
       .name("eks-blueprints-workshop-pipeline")
